4a1daacd91dfaaed154576a695cbcc069ccbc3c8
samantha-bowling <sam.g.velarde@gmail.com>
2025-05-12 10:28:42 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { SteamSessionProvider } from './hooks/useSteamSession'; // make sure the path matches your project
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <SteamSessionProvider>
+    <App />
+  </SteamSessionProvider>
+);
