75c78d7d30a5e493cc2024f133b88f080e211cae
samantha-bowling <sam.g.velarde@gmail.com>
2025-05-12 16:36:44 -0700

    Update index.ts

--- supabase/functions/steam-auth/index.ts
+++ supabase/functions/steam-auth/index.ts
@@ -1,6 +1,7 @@
+
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.39.5";
-import * as steam from "https://deno.land/x/openid_steam@v1.0.3/mod.ts";
+import * as steam from "https://deno.land/x/openid_steam@v1.0.2/mod.ts";
 
 // Required environment variables
 const SUPABASE_URL = Deno.env.get("SUPABASE_URL")!;
