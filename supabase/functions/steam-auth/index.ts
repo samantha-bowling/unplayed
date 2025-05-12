75c132f0f676bfe74221731df48c9df7aa56c79d
samantha-bowling <sam.g.velarde@gmail.com>
2025-05-11 18:29:43 -0700

    Update index.ts

--- supabase/functions/steam-auth/index.ts
+++ supabase/functions/steam-auth/index.ts
@@ -1,5 +1,5 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.49.4";
 import { v4 as uuidv4 } from "https://esm.sh/uuid@9.0.0";
 
 const SUPABASE_URL = "https://gwmygthanyycveyqqspr.supabase.co";
